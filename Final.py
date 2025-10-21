cf53d13ea7769d712e02ac2604cf1360fb167ba9
hzq-cat <hzqroslan59@gmail.com>
2025-10-21 13:22:36 +0800

    Update Final.py

--- Final.py
+++ Final.py
@@ -21,24 +21,36 @@ prod3 = 0
 ## -----------------------------------------------------------------------------------------
 
 ## SHOW PRODUCTS AS MENU -------------------------------------------------------------------
-st.title('Welcome to our Cafe interface!')
-st.write('Please select your order:')
+if page == "Menu":
+  st.title('Welcome to our Cafe interface!')
+  st.write('Please select your order:')
 
-## Seperating one line into the columns
-col1, col2, col3 = st.columns(3)
+  ## Seperating one line into the columns
+  col1, col2, col3 = st.columns(3)
 
-## Assigning to different columns
-with col1:
-  st.image('https://cdn.shopify.com/s/files/1/0669/0966/7619/files/espresso-shot-crema-in-white-cup-on-wood-table-wrexham-bean.webp?v=1745257519')
-  st.number_input("Coffee  >>>  $3.00 each", min_value=0, max_value=10, step=1, key="prod1")
+  ## Assigning to different columns
+  with col1:
+    st.image('https://cdn.shopify.com/s/files/1/0669/0966/7619/files/espresso-shot-crema-in-white-cup-on-wood-table-wrexham-bean.webp?v=1745257519')
+    st.number_input("Coffee  >>>  $3.00 each", min_value=0, max_value=10, step=1, key="prod1")
 
-with col2:
-  st.image('https://emilylaurae.com/wp-content/uploads/2022/08/passion-fruit-juice-2.jpg')
-  st.number_input("Fruit Juice  >>>  $2.00 each", min_value=0, max_value=10, step=1, key="prod2")
+  with col2:
+    st.image('https://emilylaurae.com/wp-content/uploads/2022/08/passion-fruit-juice-2.jpg')
+    st.number_input("Fruit Juice  >>>  $2.00 each", min_value=0, max_value=10, step=1, key="prod2")
 
-with col3:
-  st.image('https://static.vecteezy.com/system/resources/previews/001/738/638/large_2x/chocolate-cake-slice-free-photo.jpg')
-  st.number_input("Cake Slice  >>>  $6.00 each", min_value=0, max_value=10, step=1, key="prod3")
+  with col3:
+    st.image('https://static.vecteezy.com/system/resources/previews/001/738/638/large_2x/chocolate-cake-slice-free-photo.jpg')
+    st.number_input("Cake Slice  >>>  $6.00 each", min_value=0, max_value=10, step=1, key="prod3")
+
+  st.subheader("Choose time of day")
+  slot = st.radio("Time slot", SLOTS, index=0, horizontal=True)
+  band = slot_to_band(slot)
+  st.caption(
+      f"""• Active band: :rainbow[**{band}**] \n
+  • Morning: 20% off Coffee+Cake (combo) \n
+  • Afternoon: 20% off Fruit Juice \n
+  • Evening: 30% off everything \n
+  • Bulk: more than 3 of the same item = extra 10% off (before time discount)"""
+  )
 
 ## End of Haziq's portion -----------------------------------------------------------------
 
@@ -53,17 +65,6 @@ def slot_to_band(s: str) -> str:
         return "afternoon"
     else:
         return "evening"
-st.subheader("Choose time of day")
-slot = st.radio("Time slot", SLOTS, index=0, horizontal=True)
-band = slot_to_band(slot)
-st.caption(
-    f"""• Active band: :rainbow[**{band}**] \n
-• Morning: 20% off Coffee+Cake (combo) \n
-• Afternoon: 20% off Fruit Juice \n
-• Evening: 30% off everything \n
-• Bulk: more than 3 of the same item = extra 10% off (before time discount)"""
-)
-
 ## -----------------------------------------------------------------------------------------
 
 ## ANDY & WAI YAN'S BLOCK--------------------------------------------------------------------------
@@ -90,124 +91,125 @@ def find_price(item_name):
         if(item_name == category['Name']):
             return category['Price']
 
-## Displaying items, their prices, and discounts of items in the cart--------------------------------------------------------------------
-st.subheader("Cart (with discounts)")
-
-rows = []
-total_raw = 0.0
-total_bulk_disc = 0.0
-total_time_disc = 0.0
-grand_total = 0.0
-
-## Looping to calculate the total price, including time and bulk discounts --------------------------------------------------------------------
-for item, qty in order_now.items():
-    if qty <=0:
-        continue
-    unit = find_price(item)
-    raw = round(unit*qty,2)
-
-    before_time_disc = raw
-    if (qty >=3):
-        before_time_disc = round(raw*0.9,2)
-        
-    time_disc_fac = 0.0
-    if (band == "evening"):
-        time_disc_fac = 0.3
-    elif (band == "afternoon" and item == "Fruit Juice"):
-        time_disc_fac = 0.2
-    elif (band == "morning" and morning and item in {"Coffee", "Cake"}):
-        time_disc_fac = 0.2
-
-    time_disc = round(before_time_disc * time_disc_fac, 2)
-    after_time_disc = round(before_time_disc - time_disc, 2)
-
-    bulk_disc = round(raw - before_time_disc, 2)
-
-    ## To show a total breakdown of the discounts in a table later on --------------------------------------------------------------------
-    rows.append({
-        "Item": item,
-        "Qty": qty,
-        "Unit ($)": unit,
-        "Raw ($)": raw,
-        "Bulk - ($)": bulk_disc,
-        "Before time discount($)": before_time_disc,
-        "Time disc - ($)": time_disc,
-        "After time discount ($)": after_time_disc
-    })
-    
-    total_raw += raw
-    total_bulk_disc += bulk_disc
-    total_time_disc += time_disc
-    grand_total += after_time_disc
-
-## This is the breakdown table --------------------------------------------------------------------
-if rows:
-    st.dataframe(pd.DataFrame(rows), use_container_width=True)
-    st.markdown(
-        f"""
-**Raw total:** ${total_raw:.2f}\n 
-**Bulk discounts:** −${total_bulk_disc:.2f}\n
-**Time-band discounts:** −${total_time_disc:.2f}  
-### **Cart total now: ${grand_total:.2f}**
-"""
-    )
 else:
-    st.info("Your cart is empty. Add some items from the menu above!")
+  ## Displaying items, their prices, and discounts of items in the cart--------------------------------------------------------------------
+  st.subheader("Cart (with discounts)")
 
-st.divider()
+  rows = []
+  total_raw = 0.0
+  total_bulk_disc = 0.0
+  total_time_disc = 0.0
+  grand_total = 0.0
 
-## DISPLAYING RECEIPT AS A TABLE (KHANSKY) -------------------------------------------------
+  ## Looping to calculate the total price, including time and bulk discounts --------------------------------------------------------------------
+  for item, qty in order_now.items():
+      if qty <=0:
+          continue
+      unit = find_price(item)
+      raw = round(unit*qty,2)
 
-#actual main command that you use to pull
-def receipt(full_list):
-    #creates new list that willo be used to combine allat
-    idlist = []
-    quantitylist = []
-    pricelist = []
-    subtotal = []
+      before_time_disc = raw
+      if (qty >=3):
+          before_time_disc = round(raw*0.9,2)
+        
+      time_disc_fac = 0.0
+      if (band == "evening"):
+          time_disc_fac = 0.3
+      elif (band == "afternoon" and item == "Fruit Juice"):
+          time_disc_fac = 0.2
+      elif (band == "morning" and morning and item in {"Coffee", "Cake"}):
+          time_disc_fac = 0.2
 
-    #seperates the full_list into 4 seperate columns: id, qty, price, sub
-    for individual_items, individual_qty in full_list:
-        price = find_price(individual_items)
-        idlist.append(individual_items)
-        quantitylist.append(individual_qty)
-        pricelist.append(price)
-        subtotal.append(individual_qty*price)
+      time_disc = round(before_time_disc * time_disc_fac, 2)
+      after_time_disc = round(before_time_disc - time_disc, 2)
 
-    #panda that into something pandable
-    fullframe = pd.DataFrame({'Item':idlist, 'Quantity':quantitylist, 'Price per Item($)':pricelist, 'Subtotal($)':subtotal})
+      bulk_disc = round(raw - before_time_disc, 2)
 
-    #return allat work
-    return fullframe
+      ## To show a total breakdown of the discounts in a table later on --------------------------------------------------------------------
+      rows.append({
+          "Item": item,
+          "Qty": qty,
+          "Unit ($)": unit,
+          "Raw ($)": raw,
+          "Bulk - ($)": bulk_disc,
+          "Before time discount($)": before_time_disc,
+          "Time disc - ($)": time_disc,
+          "After time discount ($)": after_time_disc
+      })
+    
+      total_raw += raw
+      total_bulk_disc += bulk_disc
+      total_time_disc += time_disc
+      grand_total += after_time_disc
 
-## Adding a checkout button, displaying the full receipt and price breakdown at the bottom of the page --------------------------------------------------------------------
-if st.button("CHECKOUT"):
-  full_list = [(coffee['Name'], prod1), (frjuice['Name'], prod2), (cake['Name'], prod3)]
-  st.subheader("Final receipt (items & subtotals)")
-  st.write(receipt(full_list))
-
-  time.sleep(1)
-
-  # also show the same discount totals snapshot at checkout
-  st.subheader("Discount breakdown at checkout")
+  ## This is the breakdown table --------------------------------------------------------------------
   if rows:
-    st.dataframe(pd.DataFrame(rows), use_container_width=True)
-
-    time.sleep(1)
-    
-    st.markdown(f"**Raw total:** ${total_raw:.2f}\n ")
-
-    time.sleep(1)
-    
-    st.markdown(f"**Bulk discounts:** −${total_bulk_disc:.2f}\n ")
-    
-    time.sleep(1)
-    
-    st.markdown(f"**Time-band discounts:** −${total_time_disc:.2f}\n ")
-
-    time.sleep(1)
-    
-    st.markdown(f"### **Total due: ${grand_total:.2f}** ")  
-      
+      st.dataframe(pd.DataFrame(rows), use_container_width=True)
+      st.markdown(
+          f"""
+  **Raw total:** ${total_raw:.2f}\n 
+  **Bulk discounts:** −${total_bulk_disc:.2f}\n
+  **Time-band discounts:** −${total_time_disc:.2f}  
+  ### **Cart total now: ${grand_total:.2f}**
+  """
+      )
   else:
-      st.info("No items were selected at checkout.")
+      st.info("Your cart is empty. Add some items from the menu above!")
+
+  st.divider()
+
+  ## DISPLAYING RECEIPT AS A TABLE (KHANSKY) -------------------------------------------------
+
+  #actual main command that you use to pull
+  def receipt(full_list):
+      #creates new list that willo be used to combine allat
+      idlist = []
+      quantitylist = []
+      pricelist = []
+      subtotal = []
+
+      #seperates the full_list into 4 seperate columns: id, qty, price, sub
+      for individual_items, individual_qty in full_list:
+          price = find_price(individual_items)
+          idlist.append(individual_items)
+          quantitylist.append(individual_qty)
+          pricelist.append(price)
+          subtotal.append(individual_qty*price)
+
+      #panda that into something pandable
+      fullframe = pd.DataFrame({'Item':idlist, 'Quantity':quantitylist, 'Price per Item($)':pricelist, 'Subtotal($)':subtotal})
+
+      #return allat work
+      return fullframe
+
+  ## Adding a checkout button, displaying the full receipt and price breakdown at the bottom of the page --------------------------------------------------------------------
+  if st.button("CHECKOUT"):
+    full_list = [(coffee['Name'], prod1), (frjuice['Name'], prod2), (cake['Name'], prod3)]
+    st.subheader("Final receipt (items & subtotals)")
+    st.write(receipt(full_list))
+
+    time.sleep(1)
+
+    # also show the same discount totals snapshot at checkout
+    st.subheader("Discount breakdown at checkout")
+    if rows:
+      st.dataframe(pd.DataFrame(rows), use_container_width=True)
+
+      time.sleep(1)
+    
+      st.markdown(f"**Raw total:** ${total_raw:.2f}\n ")
+
+      time.sleep(1)
+    
+      st.markdown(f"**Bulk discounts:** −${total_bulk_disc:.2f}\n ")
+    
+      time.sleep(1)
+    
+      st.markdown(f"**Time-band discounts:** −${total_time_disc:.2f}\n ")
+
+      time.sleep(1)
+    
+      st.markdown(f"### **Total due: ${grand_total:.2f}** ")  
+      
+    else:
+        st.info("No items were selected at checkout.")
